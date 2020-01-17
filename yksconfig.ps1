987482be496f2e6eec6c71fb3cbde881b31a0d98
Tony Cavella <acavella@gmail.com>
2020-01-16 19:13:57 -0500

    Add Function
    LockConfig, generates and sets new Yubikey configuration lock code

--- yksconfig.ps1
+++ yksconfig.ps1
@@ -13,11 +13,31 @@
 #>
 Param
 	(
-    	[parameter(Position = 0, Mandatory = $True)]
-        [String] $Days,
-        [parameter(Position = 1, Mandatory = $False)]
-		[String] $LogFile = "C:\logfiles\",
-		[parameter(Position = 2, Mandatory = $False)]
+    	[parameter(Position = 0, Mandatory = $False)]
 		[Alias('dryrun')]
 		[Switch] $NoDisable
 	)
+# ykman Location
+$ykmanPath="C:\Program Files\Yubico\YubiKey Manager\ykman.exe"
+# USB Interface Settings
+$config_usb=@{
+	OTP = "disable";
+	U2F = "disable";
+	OPGP = "disable";
+	PIV = "enable";
+	OATH = "disable";
+	FIDO2 = "disable"
+}
+# NFC Interface Settings
+$config_nfc=@{
+	OTP = "disable";
+	U2F = "disable";
+	OPGP = "disable";
+	PIV = "disable";
+	OATH = "disable";
+	FIDO2 = "disable"
+}
+function LockConfig { # Set Configuration Lock Code
+$GenerateLockCode = (ykman.exe config set-lock-code -g -f)  | Out-String
+$LockCode = $GenerateLockCode.Split(': ',2)[-1]
+}
